3d0d04ceecfa916bc545e7dcf3c5610dcfe98f7e
John Eagle <john.p.eagle@gmail.com>
2019-01-29 20:09:13 -0800

    testing sensors

--- captain_flint.py
+++ captain_flint.py
@@ -1,6 +1,55 @@
-import subprocess
+# standard imports
+import socket, argparse, random, time, subprocess
+from datetime import datetime, timedelta
+
+# pi import
 import RPi.GPIO as GPIO
-from time import sleep
+
+# osc imports
+from pythonosc import dispatcher, osc_server, osc_message_builder, udp_client
+
+TRIG = 23
+ECHO = 24
+
+
+def ultrasonic_init():
+    GPIO.setmode(GPIO.BCM)
+
+    GPIO.setup(TRIG, GPIO.OUT)
+    GPIO.setup(ECHO, GPIO.IN)
+
+    GPIO.output(TRIG, False)
+    time.sleep(2)
+
+def get_reading():
+    # does a ping or something
+    GPIO.output(TRIG, True)
+    time.sleep(0.00001)
+    GPIO.output(TRIG, False)
+
+    break_loop = False
+    timeout = datetime.now() + timedelta(seconds=1)
+
+    pulse_end = 0
+    pulse_start = 0
+
+    # finds the time measurements?
+    while GPIO.input(ECHO)==0:
+        pulse_start = time.time()
+        # if timeout > datetime.now():
+        #    break;
+
+    timeout = datetime.now() + timedelta(seconds=1)
+    while GPIO.input(ECHO)==1:
+        pulse_end = time.time()
+        # if timeout > datetime.now():
+        #     break;
+
+    # some calculations to convert to centimeters
+    pulse_duration = pulse_end - pulse_start
+    distance = pulse_duration * 17150
+    return round(distance, 2)
+
 
 # setup
 filename = "pieces_of_eight.wav"
@@ -8,8 +57,17 @@ command = 'aplay'
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(2, GPIO.OUT) # initialize to 'off'
 
+# initialize control switch for control relay
+GPIO.setup(2, GPIO.OUT) # initialize to 'off'
+# initialize sensor pins
+ultrasonic_init()
+
+while True:
+    print(get_reading())
+    time.sleep(1)
+
+"""
 print('on')
 GPIO.output(2, True)
 subprocess.call([command, filename])
@@ -18,3 +76,4 @@ sleep(2)
 
 print('off')
 GPIO.output(2, False)
+"""
