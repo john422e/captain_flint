5d1aaf9694aa023efb9912353fc1cd5074fa9573
John Eagle <john.p.eagle@gmail.com>
2019-01-29 19:56:50 -0800

    sound and light

--- captain_flint.py
+++ captain_flint.py
@@ -1,6 +1,20 @@
 import subprocess
+import RPi.GPIO as GPIO
+from time import sleep
 
+# setup
 filename = "pieces_of_eight.wav"
 command = 'afplay'
 
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(2, GPIO.OUT) # initialize to 'off'
+
+print('on')
+GPIO.output(2, True)
 subprocess.call(['afplay', filename])
+
+sleep(2)
+
+print('off')
+GPIO.output(2, False)
